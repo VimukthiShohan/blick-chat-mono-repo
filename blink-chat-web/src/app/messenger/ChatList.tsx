fa6df43d0cc92c0a6b6f08df7410ae0dfb2f9e0e
Vimukthi Shohan Jayawardana <vimukthi.shohan@gmail.com>
2024-02-29 20:16:53 +0530

    Chatlist UX

--- blink-chat-web/src/app/messenger/ChatList.tsx
+++ blink-chat-web/src/app/messenger/ChatList.tsx
@@ -13,6 +13,7 @@ import {
 import { Nullable } from '@/api/apiService';
 import { useGetConversations } from '@/api/coversation';
 import { ConversationResponse } from '@/types/conversation.types';
+import CircularProgress from '@mui/material/CircularProgress';
 
 interface ChatListProps {
   setSelectedConversation: (chat: ConversationResponse) => void;
@@ -23,34 +24,42 @@ const ChatList: React.FC<ChatListProps> = ({
   setSelectedConversation,
   unselectConversation,
 }) => {
-  const { data: chatListData } = useGetConversations();
+  const { data: chatListData, isLoading } = useGetConversations();
 
   return (
-    <List className="overflow-y-auto">
-      {chatListData?.length === 0 ? (
+    <>
+      {isLoading ? (
         <div className="flex justify-center mt-3">
-          <Typography>No conversations yet</Typography>
+          <CircularProgress size={24} />
         </div>
       ) : (
-        chatListData?.map((chat) => (
-          <ListItem
-            key={chat.conversationId}
-            onClick={() => {
-              setSelectedConversation(chat);
-            }}
-            className={`bg-${unselectConversation?.conversationId === chat.conversationId ? 'gray-400' : 'white'} border-b border-gray-300`}
-          >
-            <ListItemAvatar>
-              <Avatar />
-            </ListItemAvatar>
-            <ListItemText
-              primary={chat.userName}
-              secondary={true ? 'Online' : 'Offline'}
-            />
-          </ListItem>
-        ))
+        <List className="overflow-y-auto">
+          {chatListData?.length === 0 ? (
+            <div className="flex justify-center mt-3">
+              <Typography>No conversations yet</Typography>
+            </div>
+          ) : (
+            chatListData?.map((chat) => (
+              <ListItem
+                key={chat.conversationId}
+                onClick={() => {
+                  setSelectedConversation(chat);
+                }}
+                className={`bg-${unselectConversation?.conversationId === chat.conversationId ? 'gray-400' : 'white'} border-b border-t border-gray-300`}
+              >
+                <ListItemAvatar>
+                  <Avatar />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={chat.userName}
+                  secondary={true ? 'Online' : 'Offline'}
+                />
+              </ListItem>
+            ))
+          )}
+        </List>
       )}
-    </List>
+    </>
   );
 };
 
