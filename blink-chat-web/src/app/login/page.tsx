986491516122b7c9a23cb30e7dab9f304c3a0092
Vimukthi Shohan Jayawardana <vimukthi.shohan@gmail.com>
2024-03-04 11:17:28 +0530

    Page headers

--- blink-chat-web/src/app/login/page.tsx
+++ blink-chat-web/src/app/login/page.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from 'next';
+
 import LoginForm from '@/app/login/loginForm';
 import AuthLayout from '@/components/common/AuthLayout';
+
+export const metadata: Metadata = { title: 'Login' };
 
 const LoginPage = () => (
   <AuthLayout>
--- blink-chat-web/src/app/signup/page.tsx
+++ blink-chat-web/src/app/signup/page.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from 'next';
+
 import SignUpForm from '@/app/signup/signUpForm';
 import AuthLayout from '@/components/common/AuthLayout';
+
+export const metadata: Metadata = { title: 'Signup' };
 
 const SignUpPage = () => (
   <AuthLayout>
